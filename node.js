06e303ee296fab4669d7e72714c97799945e0545
JPbooya <perezjcarlos81@gmail.com>
2026-02-10 15:31:58 -0800

    added route

--- node.js
+++ node.js
@@ -1,19 +1,6 @@
 import express from 'express';
 const app = express();
 const PORT = 3001;
-
-// accessing through the main directory
-app.use(express.static(import.meta.dirname));
-
-app.get('/', (req, res) => {
-  res.sendFile(`${import.meta.dirname}/index.html`);
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
-});
-
-
 app.use(express.static(import.meta.dirname));
 
 app.get('/', (req, res) => {
@@ -21,5 +8,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running at https://localhost:${PORT}`);
+   console.log(`Server is running at https://localhost:${PORT}`);
 });
